2d3fad013c7aab846ec90a05675a66732d37d8ca
Jostein Leira <9363084+josteinl@users.noreply.github.com>
2023-11-09 11:03:24 +0100

    Add error message if the KOF file contains TABs (#16)
    * - Add error message if KOF file contains tabs.
    - Update packages
    - Update CHANGES.md
    - Bump version number to 0.1.1
    
    * - Fix black issue.

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,6 +1,6 @@
 import pytest
 
-from kof_parser import KOFParser
+from kof_parser import KOFParser, exceptions
 from kof_parser.enums import MethodType
 
 
@@ -103,3 +103,14 @@ class TestParse:
 
         for method in location7.methods:
             assert method == MethodType.PZ.value
+
+    def test_err_file_containing_tabs(self):
+
+        srid = 5110
+        parser = KOFParser()
+
+        with pytest.raises(
+            exceptions.ParseError,
+            match="Error parsing KOF file on line 3 - KOF file contains tabs, please convert to spaces.",
+        ):
+            parser.parse("tests/data/kof-with-tabs.kof", srid)
